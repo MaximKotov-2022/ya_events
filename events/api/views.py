5fd2a7e4c7a5e069b1c7a610fdaf9e750ab77381
Maxim Kotov <KM4252010@yandex.ru>
2023-10-17 21:58:24 +0300

    Add a ViewSet for the API to work. Create adding event data to the database.

--- events/api/views.py
+++ events/api/views.py
@@ -1,8 +1,25 @@
-from django.http import HttpResponse
+from rest_framework.viewsets import ModelViewSet
 
 from .get_data_parsing import processing_data_website
+from .models import Event
+from .serializers import EventSerializer
 
 
-def index(request):
+class EventViewSet(ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+    # Получаем данные после парсинга и добавляем (или обновляем) данных в БД.
     data = processing_data_website()
-    return HttpResponse(f'Главная страница1111 {data}')
+    for event_data in data:
+        name = event_data['name'].text
+        site = event_data['site']
+        date = event_data['date']
+        obj, created = Event.objects.get_or_create(name=name,
+                                                   site=site,
+                                                   date=date)
+        if not created:
+            setattr(obj, 'site', site)
+            obj.save()
+
+
