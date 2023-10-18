47855e1c5c3905aa14daad85f9b30fc9aa47d263
Maxim Kotov <KM4252010@yandex.ru>
2023-10-18 20:08:51 +0300

    Add API documentation.

--- events/events/settings.py
+++ events/events/settings.py
@@ -17,6 +17,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    # 'rest_framework.schemas',
     'api.apps.ApiConfig',
 ]
 
--- events/events/urls.py
+++ events/events/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from rest_framework.schemas import get_schema_view
+from django.urls import path
+
+
+schema_view = get_schema_view(title="Example API")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/events/', include('api.urls')),
+    path('schema/', schema_view),
 ]
