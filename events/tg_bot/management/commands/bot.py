4ebc2b58b72f646696d76465ef2bf70ed59958c9
Maxim Kotov <KM4252010@yandex.ru>
2023-10-23 21:58:24 +0300

    Improve readability. Add docstrings. Change the command handlers.

--- events/tg_bot/management/commands/bot.py
+++ events/tg_bot/management/commands/bot.py
@@ -16,7 +16,8 @@ from tg_bot.models import Profile, Subscription
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
-RETRY_PERIOD = 1800
+# RETRY_PERIOD = 1800
+RETRY_PERIOD = 2
 
 
 def process_information_parsing() -> str:
@@ -30,15 +31,14 @@ def process_information_parsing() -> str:
 
     response = requests.get('http://127.0.0.1:8000/api/v1/events/')
     if response.status_code == 200:
-        data = (
-            requests.get(url='http://127.0.0.1:8000/api/v1/events/')
-        ).json()
+        data = requests.get(url='http://127.0.0.1:8000/api/v1/events/').json()
+
     text = []
     locale.setlocale(locale.LC_ALL, '')
+
     for event in data:
         date = datetime.datetime.strptime(
-            event['date'], "%Y-%m-%d"
-        ).date()
+            event['date'], "%Y-%m-%d").date()
         name = event['name']
         site = event['site']
         text.append(f'Дата: {date}\nНазвание: {name}\nСайт: {site}\n\n\n')
@@ -46,13 +46,23 @@ def process_information_parsing() -> str:
     return ''.join(text)
 
 
-def send_message(self, context, text):
-    """Отправка сообщений.
-
-    Принимает текст сообщения.
+def send_message(chat_id, context, text):
     """
+    Отправка сообщений.
 
-    chat = self.effective_chat
+    Принимает текст сообщения и отправляет его в указанный чат с использованием
+    переданного объекта контекста.
+
+    :param chat_id: идентификатор чата, в который будет отправлено сообщение.
+    :type chat_id: int
+    :param context: Объект контекста, предоставляющий доступ к ресурсам и
+    информации, связанным с текущим обработчиком сообщений.
+    :type context: telegram.ext.CallbackContext
+    :param text: Текст сообщения, которое нужно отправить.
+    :type text: str
+    :return: Возвращает 0 в случае успешной отправки сообщения.
+    :rtype: int
+    """
 
     buttons = ReplyKeyboardMarkup([
         ['Все мероприятия'],
@@ -62,32 +72,54 @@ def send_message(self, context, text):
     )
 
     context.bot.send_message(
-        chat_id=chat.id,
+        chat_id=chat_id,
         text=text,
         reply_markup=buttons,
         disable_web_page_preview=True,
         parse_mode=ParseMode.HTML,
     )
+
     return 0
 
 
-def all_events(self, context):
-    """Актуальные мероприятия."""
+def all_events(update: Update, context: CallbackContext):
+    """
+    Обработка команды 'Все мероприятия'.
 
-    text = process_information_parsing()
+    :param update: Обновление.
+    :type update: telegram.Update
+    :param context: Контекст.
+    :type context: telegram.ext.callbackcontext.CallbackContext
+    :return: Возвращает 0 в случае успешной отправки сообщения.
+    :rtype: int
+    """
 
-    return send_message(
-            self,
-            context,
-            text=text,
-        )
+    chat_id = update.message.chat_id
+
+    Subscription.objects.get_or_create(
+        profile__external_id=chat_id,
+        defaults={
+            'profile': Profile.objects.get_or_create(
+                external_id=update.message.chat_id,
+                name=update.effective_user.username)[0],
+            'subscription': False
+        }
+    )
+    update.message.reply_text(process_information_parsing())
+
+    return 0
 
 
-def checking_data_changes(self, context):
-    """Проверка изменений/обновлений данных с сайта.
+def checking_data_changes() -> bool:
+    """
+    Проверка изменений/обновлений данных с сайта.
 
     Если данные изменились, то возвращается True
-    Если нет - False."""
+    Если нет - False.
+
+    :return: Логическое значение, указывающее, есть ли какие-либо обновления
+    :rtype: bool
+    """
 
     while True:
         old_data = process_information_parsing()
@@ -100,11 +132,17 @@ def checking_data_changes(self, context):
         return False
 
 
-def hi_say_first_message(self, context):
-    """Отправка первого сообщения.
+def hi_say_first_message(update: Update, context: CallbackContext):
+    """
+    Отправка первого сообщения.
 
-    Получение инфо о возможностях бота."""
+    Получение инфо о возможностях бота.
 
+    :param update: Объект обновления.
+    :param context: Контекст обратного вызова.
+    :return: Результат работы функции send_message.
+    """
+    chat_id = update.effective_chat.id
     text = ("Привет! Это бот для получения информации о событиях "
             "Яндекса.\n"
 
@@ -116,37 +154,118 @@ def hi_say_first_message(self, context):
             "<u>Отписаться</u> - отписаться от уведомлений."
             )
 
-    return send_message(
-            self,
-            context,
-            text=text,
-        )
+    return send_message(chat_id,
+                        context,
+                        text=text,)
+
+
+def subscribe(update: Update, context: CallbackContext):
+    """
+    Подписка пользователя на получение обновлений.
+
+    :param update: Объект обновления.
+    :param context: Контекст обратного вызова.
+    :return: Возвращает 0 в случае успешной отправки сообщения.
+    :rtype: Int
+    """
+    chat_id = update.effective_chat.id
+
+    subscription, created = Subscription.objects.get_or_create(
+        profile__external_id=chat_id,
+        defaults={
+            'profile': Profile.objects.get_or_create(
+                external_id=update.message.chat_id,
+                name=update.effective_user.username)[0],
+            'subscription': True
+        }
+    )
+
+    if subscription.subscription:
+        update.message.reply_text('Вы уже подписаны на обновления')
+    else:
+        # Если подписка уже есть, то обновляем значение поля subscription в БД
+        subscription.subscription = True
+        subscription.save()
+        update.message.reply_text('Вы успешно подписались на обновления')
+
+    return 0
+
+
+def unsubscribe(update: Update, context: CallbackContext):
+    """
+    Отписка пользователя от получений обновлений.
+
+    :param update: Объект обновления.
+    :param context: Контекст обратного вызова.
+    :return: Возвращает 0 в случае успешной отправки сообщения.
+    :rtype: Int
+    """
+    chat_id = update.effective_chat.id
+
+    subscription, created = Subscription.objects.get_or_create(
+        profile__external_id=chat_id,
+        defaults={
+            'profile': Profile.objects.get_or_create(
+                external_id=update.message.chat_id,
+                name=update.effective_user.username)[0],
+            'subscription': False
+        }
+    )
+
+    if subscription.subscription:
+        # Если подписка уже есть, то обновляем значение поля subscription в БД
+        subscription.subscription = False
+        subscription.save()
+        update.message.reply_text('Вы отписались от обновлений')
+    else:
+        update.message.reply_text('Вы не подписаны на обновления')
+
+    return 0
 
 
 def do_echo(update: Update, context: CallbackContext):
-    chat_id = update.message.chat_id
-    text = update.message.text
+    """
+    Обрабатывает текст, которого нет в командах.
+    Добавляет данные о пользователе в БД.
 
-    obj, created = Profile.objects.get_or_create(
-        external_id=chat_id,
+    :param update: Объект обновления.
+    :param context: Контекст обратного вызова.
+    :return: Возвращает 0 в случае успешной отправки сообщения.
+    :rtype: int
+    """
+    chat_id = update.message.chat_id
+
+    Subscription.objects.get_or_create(
+        profile__external_id=chat_id,
         defaults={
-            'name': update.message.from_user.username,
+            'profile': Profile.objects.get_or_create(
+                external_id=update.message.chat_id,
+                name=update.effective_user.username)[0],
+            'subscription': False
         }
     )
-    # sub, created = Subscription.objects.update(
-    #     external_id=chat_id,
-    #     subscription=subscription_status,
-    # )
-    data = str(process_information_parsing())
-    update.message.reply_text(
-        text=data,
-    )
+
+    update.message.reply_text(text='Неизвестная команда')
+
+    return 0
 
 
 class Command(BaseCommand):
+    """Команды Telegram-бота."""
+
     help = 'Телеграмм-бот'
 
     def handle(self, *args, **kwargs):
+        """
+        Обработчик команд бота.
+
+        :param args: Аргументы команды.
+        :type args: Any
+        :param kwargs: Аргументы ключевого слова команды.
+        :type kwargs: Any
+        :return: None
+        """
+
         # Подключение
         request = Request(
             connect_timeout=0.5,
@@ -163,9 +282,9 @@ class Command(BaseCommand):
             use_context=True,
 
         )
-        message_handler = MessageHandler(Filters.text, do_echo)
         updater = Updater(token=TOKEN)
 
+        # Обработки команд
         updater.dispatcher.add_handler(CommandHandler(
             'start',
             hi_say_first_message,
@@ -178,19 +297,21 @@ class Command(BaseCommand):
             run_async=True,
         )
         )
-        # updater.dispatcher.add_handler(MessageHandler(
-        #     Filters.text('Подписаться'),
-        #     subscribe_updates,
-        #     run_async=True,
-        # )
-        # )
-        # updater.dispatcher.add_handler(MessageHandler(
-        #     Filters.text('Отписаться'),
-        #     unsubscribe,
-        #     run_async=True,
-        # )
-        # )
+        updater.dispatcher.add_handler(MessageHandler(
+            Filters.text('Подписаться'),
+            subscribe,
+            run_async=True,
+        )
+        )
+        updater.dispatcher.add_handler(MessageHandler(
+            Filters.text('Отписаться'),
+            unsubscribe,
+            run_async=True,
+        )
+        )
 
+        # Обработчик сообщений по умолчанию
+        message_handler = MessageHandler(Filters.text, do_echo)
         updater.dispatcher.add_handler(message_handler)
 
         updater.start_polling()
