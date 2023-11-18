0fc9f24d6f196daf2d1b5475c5a9aac729d324f1
Maxim Kotov <KM4252010@yandex.ru>
2023-11-18 21:51:11 +0300

    Добавлена отправка писем на email

--- events/api/views.py
+++ events/api/views.py
@@ -1,3 +1,8 @@
+import os
+
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from dotenv import load_dotenv
 from rest_framework.permissions import SAFE_METHODS
 from rest_framework.viewsets import ModelViewSet
 
@@ -5,6 +10,22 @@ from .get_data_parsing import processing_data_website
 from .models import Event
 from .permissions import AdminOnly, ReadOnly
 from .serializers import EventSerializer
+
+load_dotenv()
+
+
+def email_post_message():
+    subject = 'Письмо с новыми мероприятиями'
+    from_email = os.getenv('EMAIL_HOST_USER')
+    message = ('Здравствуйте! Приглашаем вас на наше мероприятие'
+               'Будем рады видеть вас там!')
+    recipient_list = [os.getenv('EMAIL_HOST_USER'), ]
+    msg_html = render_to_string('email.html',
+                                {'title': subject,
+                                 'message': message})
+
+    send_mail(subject, message, from_email, recipient_list,
+              fail_silently=False, html_message=msg_html)
 
 
 class EventViewSet(ModelViewSet):
@@ -47,6 +68,7 @@ class EventViewSet(ModelViewSet):
         return 0
 
     def list(self, request):
+        email_post_message()
         self.delete_missing_events()
         return super().list(request)
 
@@ -65,3 +87,4 @@ class EventViewSet(ModelViewSet):
     def destroy(self, request, pk=None):
         self.delete_missing_events()
         return super().destroy(request, pk)
+
--- events/events/settings.py
+++ events/events/settings.py
@@ -104,3 +104,11 @@ USE_TZ = os.getenv('USE_TZ', True)
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL')
