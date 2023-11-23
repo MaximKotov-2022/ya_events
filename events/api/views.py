3780c4881e8839cec1869fe4c43c62a3c374cb8a
Maxim Kotov <KM4252010@yandex.ru>
2023-11-23 20:47:04 +0300

    Изменены метода ViewSet для исправления ошибки при миграциях

--- events/api/views.py
+++ events/api/views.py
@@ -11,6 +11,7 @@ from .models import Event
 from .permissions import AdminOnly, ReadOnly
 from .serializers import EventSerializer
 
+
 load_dotenv()
 
 
@@ -37,19 +38,6 @@ class EventViewSet(ModelViewSet):
             return [ReadOnly()]
         return [AdminOnly()]
 
-    # Получаем данные после парсинга и добавляем (или обновляем) данных в БД.
-    data = processing_data_website()
-    for event_data in data:
-        name = event_data['name'].text
-        site = event_data['site']
-        date = event_data['date']
-        obj, created = Event.objects.get_or_create(name=name,
-                                                   site=site,
-                                                   date=date)
-        if not created:
-            setattr(obj, 'site', site)
-            obj.save()
-
     def delete_missing_events(self):
         """Получаем названия мероприятий, которых больше нет на сайте с
         мероприятиями, но есть в БД.
@@ -67,24 +55,21 @@ class EventViewSet(ModelViewSet):
 
         return 0
 
-    def list(self, request):
-        email_post_message()
+    def list(self, request, *args, **kwargs):
+        """Получаем данные после парсинга и добавляем (или обновляем)
+        данных в БД.
+        """
+        data_from_the_site = processing_data_website()
+        for event_data in data_from_the_site:
+            name = event_data['name'].text
+            site = event_data['site']
+            date = event_data['date']
+
+            obj, created = Event.objects.get_or_create(name=name,
+                                                       site=site,
+                                                       date=date)
+            if not created:
+                setattr(obj, 'site', site)
+                obj.save()
         self.delete_missing_events()
         return super().list(request)
-
-    def create(self, request):
-        self.delete_missing_events()
-        return super().create(request)
-
-    def update(self, request, pk=None):
-        self.delete_missing_events()
-        return super().update(request, pk)
-
-    def partial_update(self, request, pk=None):
-        self.delete_missing_events()
-        return super().partial_update(request, pk)
-
-    def destroy(self, request, pk=None):
-        self.delete_missing_events()
-        return super().destroy(request, pk)
-
